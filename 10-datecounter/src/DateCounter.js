d9b72cbfa21bb1c27e9e701deb2966295a696a0f
AmigoThin <amigothin2swe@gmail.com>
2026-07-03 16:34:36 +0630

    10-datecounter-p_1

---
+++ 10-datecounter/src/DateCounter.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+function DateCounter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  function dec() {
+    setCount((c) => c - step);
+  }
+
+  function inc() {
+    setCount((c) => c + step);
+  }
+
+  function defineStep(e) {
+    setStep(Number(e.target.value));
+  }
+
+  function defineCount(e) {
+    setCount(Number(e.target.value));
+  }
+
+  function reset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div className="counter">
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={defineStep}
+        />
+        <span>{step}</span>
+      </div>
+
+      <div>
+        <button onClick={dec}>-</button>
+        <input value={count} onChange={defineCount} />
+        <button onClick={inc}>+</button>
+      </div>
+
+      <p>{date.toDateString()}</p>
+
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default DateCounter;
